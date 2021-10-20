e41d63d4cff456ee9a99db8f65230fbf13f64a9a
Maxim Bayurov <m.bayurov@agimagroup.ru>
2021-10-20 20:35:49 +0300

    1.10 Сервисы и Autowiring

--- cat_cas_car/src/Controller/ArticleLikeController.php
+++ cat_cas_car/src/Controller/ArticleLikeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,12 +13,14 @@ class ArticleLikeController extends AbstractController
      * @Route("/articles/{id<\d+>}/like/{type<like|dislike>}", methods={"POST"}, name="app_article_like")
      *
      */
-    public function like($id, $type): JsonResponse
+    public function like($id, $type, LoggerInterface $logger): JsonResponse
     {
         if ($type === 'like') {
             $likes = rand(121, 300);
+            $logger->info('Произошёл лайк!');
         } else {
             $likes = rand(0, 119);
+            $logger->info('Произошла открутка!');
         }
         
         return $this->json([
