a6f6aeeb4b5dc015f76b5eaa46b72db31fa62d70
Maxim Bayurov <m.bayurov@agimagroup.ru>
2021-10-23 21:30:39 +0300

    2.4 Продвинутое конфигурирование, service.yaml

--- cat_cas_car/src/Service/MarkdownParser.php
+++ cat_cas_car/src/Service/MarkdownParser.php
@@ -11,20 +11,30 @@ class MarkdownParser
     private Parsedown $parseDown;
     private AdapterInterface $cache;
     private LoggerInterface $logger;
+    private bool $debug;
     
-    public function __construct( Parsedown $parseDown, AdapterInterface $cache, LoggerInterface $logger)
-    {
+    public function __construct(
+        Parsedown $parseDown,
+        AdapterInterface $cache,
+        LoggerInterface $markdownLogger,
+        bool $debug
+    ) {
         $this->parseDown = $parseDown;
         $this->cache = $cache;
-        $this->logger = $logger;
+        $this->logger = $markdownLogger;
+        $this->debug = $debug;
     }
     
     public function parse(string $source): string
     {
-        if(stripos($source, 'красн') !== false) {
+        if (stripos($source, 'красн') !== false) {
             $this->logger->info(
                 'Кажется это статья о красной точке!'
             );
+        }
+        
+        if ($this->debug) {
+            return $this->parseDown->text($source);
         }
         
         return $this->cache->get(
